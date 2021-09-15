0328f39bdd3c14b6d881d3452365862e5d781dfa
Piotr Orczykowski <orczykowski.peter@gmail.com>
2021-09-15 20:55:30 +0200

    Added function to add new items

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,25 @@ $(document).ready(function()
     {
         $('.item').remove();
     });
-
-    $('.fa-check-circle').click(function()
+ 
+    $('#items').on('click', '.fa-check-circle', function()
     {
         $(this).closest('.item').find('.item-name').css({'text-decoration': 'line-through', 'color': '#cfc8c8'});
     });
 
-    $('.fa-times-circle').click(function()
+    $('#items').on('click', '.fa-times-circle', function()
     {
         $(this).closest('.item').remove();
+    });
+
+    let start = '<div class="item">\n<span class="item-name">'
+    let end = '</span>\n<div class="icons">\n   <i class="fas fa-check-circle"></i>\n   <i class="fas fa-edit"></i>\n   <i class="fas fa-times-circle"></i>\n</div>'
+    $('#add-button').on('click', function()
+    {
+        if($('#new-item').val() != '')
+        {
+            let item = start + $('#new-item').val() + end;
+            $('#items').append(item);
+        }
     });
 })
