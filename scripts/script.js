5a398f263e7e2cdb670ba5de385e5df70961cb53
Piotr Orczykowski <orczykowski.peter@gmail.com>
2021-09-16 15:41:11 +0200

    Added second function to add new items and adde minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -15,14 +15,30 @@ $(document).ready(function()
         $(this).closest('.item').remove();
     });
 
-    let start = '<div class="item">\n<span class="item-name">'
-    let end = '</span>\n<div class="icons">\n   <i class="fas fa-check-circle"></i>\n   <i class="fas fa-edit"></i>\n   <i class="fas fa-times-circle"></i>\n</div>'
+    
+    function addItem()
+    {
+        let start = '<div class="item">\n<span class="item-name">'
+        let end = '</span>\n<div class="icons">\n   <i class="fas fa-check-circle"></i>\n   <i class="fas fa-edit"></i>\n   <i class="fas fa-times-circle"></i>\n</div>'
+        let item = start + $('#new-item').val() + end;
+        $('#items').append(item);
+        $('#new-item').val('');
+    }
+
     $('#add-button').on('click', function()
     {
         if($('#new-item').val() != '')
         {
-            let item = start + $('#new-item').val() + end;
-            $('#items').append(item);
+            addItem();
+        }
+    });
+
+    $('#new-item').keypress(function()
+    {
+        let keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13' && $('#new-item').val() != '')
+        {
+            addItem();
         }
     });
 })
